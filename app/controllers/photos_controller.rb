0299a095b27d80725a4be812a6fb454c150787d1
MC <18798805737@139.com>
2017-04-21 18:21:19 +0800

    给相册控制器添加注释

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,11 @@
 class PhotosController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!#添加用户鉴权（即登录才可以查看相册内容）
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
 
   # GET /photos
   # GET /photos.json
   def index
-    @photos = Photo.all
+    @photos = Photo.all #获取所有相册内容
   end
 
   # # GET /photos/1
@@ -15,7 +15,7 @@ class PhotosController < ApplicationController
 
   # GET /photos/new
   def new
-    @photo = Photo.new
+    @photo = Photo.new #上传图片到相册
   end
 
   # # GET /photos/1/edit
@@ -25,14 +25,14 @@ class PhotosController < ApplicationController
   # POST /photos
   # POST /photos.json
   def create
-    @photo = Photo.new(photo_params)
+    @photo = Photo.new(photo_params) #接收view传的图片
 
     respond_to do |format|
-      if @photo.save
-        format.html { redirect_to photos_url, notice: 'Photo was successfully created.' }
+      if @photo.save #保存图片到相册
+        format.html { redirect_to photos_url, notice: 'Photo was successfully created.' }#保存成功，跳转到相册列表页面
         # format.json { render :show, status: :created, location: @photo }
       else
-        format.html { render :new }
+        format.html { render :new }#保存失败，跳转到上传图片页面从新上传图片。
         # format.json { render json: @photo.errors, status: :unprocessable_entity }
       end
     end
@@ -55,9 +55,9 @@ class PhotosController < ApplicationController
   # DELETE /photos/1
   # DELETE /photos/1.json
   def destroy
-    @photo.destroy
+    @photo.destroy #删除选中的相册内容
     respond_to do |format|
-      format.html { redirect_to photos_url, notice: 'Photo was successfully destroyed.' }
+      format.html { redirect_to photos_url, notice: 'Photo was successfully destroyed.' }#删除成功，跳转到相册列表页面
       # format.json { head :no_content }
     end
   end
@@ -65,11 +65,11 @@ class PhotosController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_photo
-      @photo = Photo.find(params[:id])
+      @photo = Photo.find(params[:id]) #获取选中的相册内容，用于删除操作
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:title, :description, :picture)
+      params.require(:photo).permit(:title, :description, :picture) #获取view上传的相册内容
     end
 end
