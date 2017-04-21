28de39f9037e3f0343614dd373e7beeac11f0c3f
MC <18798805737@139.com>
2017-04-21 18:02:08 +0800

    给评论的控制器添加注释

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,17 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!
- def create
-  
-    @students_forum = StudentsForum.find(params[:students_forum_id])
-    @comment = @students_forum.comments.create(comment_params)
+  before_action :authenticate_user! # 添加用户鉴权（即登录才可以参与评论）
 
-    # @comment.user_id = current_user.id
-    redirect_to students_forum_path(@students_forum)
+  # 新建评论的方法
+  def create
+    @students_forum = StudentsForum.find(params[:students_forum_id]) # 获取需要评论的话题
+    @comment = @students_forum.comments.create(comment_params) # 对获取的话题创建评论
+    # @comment.user_id = current_user.id  #评论是当前登录的用户创建的
+    redirect_to students_forum_path(@students_forum) # 评论创建后跳转到话题论坛页面
   end
 
   private
-    
-    def comment_params
-      params.require(:comment).permit(:content)
-    end
+
+  def comment_params # 获取评论的参数方法
+    params.require(:comment).permit(:content) # 评论comment只有一个内容content参数
+  end
 end
